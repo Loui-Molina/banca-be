3f5d187f20a1da8fc31fcb2f40d56620536464a1
Gonzalo Catrina <gonzalocatrina@gmal.com>
2020-12-21 14:44:30 -0300

    Added swagger annotations for  auth credentials

--- src/modules/auth/dtos/auth.credentials.dto.ts
+++ src/modules/auth/dtos/auth.credentials.dto.ts
@@ -1,20 +1,24 @@
 import { Roles } from "@database/datamodels/enums/Roles";
+import { ApiProperty } from "@nestjs/swagger";
 import { ConstApp } from "@utils/const.app";
 import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 
 export class AuthCredentialsDto{
 
+    @ApiProperty({type:String})
     @IsString()
     @MinLength(4)
     @MaxLength(20)
     username: string;
 
+    @ApiProperty({type:String})
     @IsString()
     @MinLength(8)
     @MaxLength(35)
     @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message:ConstApp.PASSWORD_MESSAGE})
     password: string;
 
+    @ApiProperty({type:Roles})
     role:Roles;
 }
