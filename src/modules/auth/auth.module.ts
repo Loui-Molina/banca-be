f5972481ce491d39ff9147517f9c5f203cab0be2
Sebastian <sebin1997@hotmail.com>
2021-01-02 17:54:24 -0300

    Final fix este si guacho

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,13 +2,13 @@ import { forwardRef, Global, Module } from '@nestjs/common';
 import { UsersModule } from '@users/users.module';
 import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
-import {JwtModule, JwtService} from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from '@auth/jwt.strategy';
 import { ConfigService } from '@nestjs/config';
 import {MongooseModule} from "@nestjs/mongoose";
 import {User, UserSchema} from "@database/datamodels/schemas/User";
-import {JWT_MODULE_OPTIONS} from "@nestjs/jwt/dist/jwt.constants";
+import {JwtStrategy} from "@auth/jwt.strategy";
+
 
 @Global()
 @Module({
@@ -17,7 +17,6 @@ import {JWT_MODULE_OPTIONS} from "@nestjs/jwt/dist/jwt.constants";
         PassportModule.register({
             defaultStrategy: 'jwt',
         }),
-        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }], 'user'), forwardRef(() => AuthModule),
         JwtModule.registerAsync({
             useFactory: async (configService: ConfigService) => {
                 return {
@@ -29,13 +28,11 @@ import {JWT_MODULE_OPTIONS} from "@nestjs/jwt/dist/jwt.constants";
             },
             inject: [ConfigService],
         }),
-        UsersModule
     ],
-    providers: [AuthService],
-    controllers: [AuthController], 
-    exports: [JwtModule, AuthService,
-        PassportModule.register({
+    providers: [AuthService, JwtStrategy],
+    controllers: [AuthController],
+    exports: [JwtModule, AuthService, PassportModule.register({
         defaultStrategy: 'jwt',
-    })],
+    }), JwtStrategy],
 })
 export class AuthModule {}
