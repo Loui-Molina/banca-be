d15ce211d31800b345003a15512ecdf8fb75114f
Gonzalo Catrina <gonzalocatrina@gmal.com>
2021-01-02 16:41:13 -0300

    Change file names

--- src/modules/consortiums/consortium.service.ts
+++ src/modules/consortiums/consortium.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
-import {Consortium, ConsortiumDocument} from '@src/modules/database/datamodels/schemas/consortiums/consortium';
+import {Consortium, ConsortiumDocument} from '@src/modules/database/datamodels/schemas/consortium';
 import {ConsortiumDto} from '@src/modules/consortiums/dtos/consortium.dto';
 
 @Injectable()
