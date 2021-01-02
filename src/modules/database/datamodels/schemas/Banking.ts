ed6f453179c0fcab47b9a6dcd4409e5d024299b2
Axel <loui.molina1@gmail.com>
2021-01-02 18:03:52 -0300

    Add owner Id to banking schema

--- src/modules/database/datamodels/schemas/Banking.ts
+++ src/modules/database/datamodels/schemas/Banking.ts
@@ -1,16 +1,18 @@
 import { DataObject } from '@src/modules/database/datamodels/schemas/data.object';
 import { BankingPreference, BankingPreferenceSchema } from '@src/modules/database/datamodels/schemas/banking.preference';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import {Document, ObjectId} from 'mongoose';
 import {Transaction, TransactionSchema} from "@database/datamodels/schemas/Transaction";
 import {Bet, BetSchema} from "@database/datamodels/schemas/Bet";
 import {Lottery, LotterySchema} from "@database/datamodels/schemas/Lottery";
+import * as mongoose from "mongoose";
+import {UserSchema} from "@database/datamodels/schemas/User";
 
 export type BankingDocument = Banking & Document;
 
 @Schema()
 export class Banking implements DataObject {
-    // @Prop({type: mongoose.Schema.Types.ObjectId, ref: UserSchema}) owner: User; // TODO CHECK
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: UserSchema}) owner: ObjectId;
     @Prop({ type: BankingPreferenceSchema })
     bankingPreferences?: BankingPreference;
     @Prop({ type: [TransactionSchema] }) transactions?: Transaction[];
