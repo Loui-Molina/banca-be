176302fac1c0426699af64c0c0f4a417f0ff7027
Sebastian Imvinkelried <sebin1997@hotmail.com>
2021-01-05 14:44:45 -0300

    Table banking in consortiums

--- src/modules/consortiums/dtos/consortium.dto.ts
+++ src/modules/consortiums/dtos/consortium.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ObjectId } from 'mongoose';
+import {Prop} from "@nestjs/mongoose";
+import {Banking, BankingSchema} from "@database/datamodels/schemas/banking";
 
 export class ConsortiumDto {
     @ApiProperty() ownerName: string;
@@ -10,4 +12,5 @@ export class ConsortiumDto {
     @ApiProperty() createdAt: Date;
     @ApiProperty() status: boolean;
     @ApiProperty() firstTransactionDate: Date;
+    @ApiProperty() bankings?: Banking[];
 }
