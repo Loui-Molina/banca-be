4d2110c9ccab9856e83902a495b4d99e2d45d206
Axel <loui.molina1@gmail.com>
2021-01-11 00:24:41 -0300

    Apply format to code Optimize imports on some clases

--- src/modules/banking/dto/update.banking.dto.ts
+++ src/modules/banking/dto/update.banking.dto.ts
@@ -1,9 +1,8 @@
 import { ObjectId } from 'mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { Languages } from '@database/datamodels/enums/languages';
 
 export class UpdateBankingDto {
-    @ApiProperty({ required: false }) _id: ObjectId;
+    @ApiProperty({ required: true }) _id: ObjectId;
     @ApiProperty() name: string;
     @ApiProperty() status: boolean;
     @ApiProperty({ required: false }) ownerUserId: ObjectId;
