1b16f07b4036e8565b6753da8956c683da6c31a2
Gonzalo Catrina <gonzalocatrina@gmal.com>
2021-01-30 22:24:09 -0300

    Fix: validationPipe to cannot return type of User

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
+
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@src/app.module';
 import { AnyExceptionFilter } from '@common/filters/any.exception.filter';
+import { ValidationPipe } from '@common/validation.pipe';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
