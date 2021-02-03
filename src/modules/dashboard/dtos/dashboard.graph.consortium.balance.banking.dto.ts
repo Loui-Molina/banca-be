b2ef4b8fb4f9b27fd9d672dbc5001ceebc201794
Sebastian Imvinkelried <sebin1997@hotmail.com>
2021-02-02 22:36:30 -0300

    BAPP 58

---
+++ src/modules/dashboard/dtos/dashboard.graph.consortium.balance.banking.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {IsArray, IsNumber, IsString} from 'class-validator';
+import {DashboardGraphBalanceBankingDto} from "@dashboard/dtos/dashboard.graph.balance.banking.dto";
+
+export class DashboardGraphConsortiumBalanceBankingDto {
+    @ApiProperty() @IsString() name: string;
+    @ApiProperty() @IsArray() series: DashboardGraphBalanceBankingDto[];
+}
