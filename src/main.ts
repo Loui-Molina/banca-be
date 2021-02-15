c488ef6ebaa6b71c19640b7ba8dc5453e57bc7e2
Gonzalo Catrina <gonzalocatrina@gmal.com>
2021-02-15 14:15:21 -0300

    mongoupdate

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@src/app.module';
 import { AnyExceptionFilter } from '@common/filters/any.exception.filter';
 import { ValidationPipe } from '@common/pipes/validation.pipe';
+import * as Express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = Express();
+server.get('/',(req,res)=> res.send('ok'));
+server.get('/_ah/health',(req,res)=> res.send('ok'));
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
     app.enableCors();
     app.setGlobalPrefix(app.get('ConfigService').get('APP_GLOBAL_PREFIX'));
     app.useGlobalFilters(new AnyExceptionFilter());
