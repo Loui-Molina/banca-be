7b82e563c4547a2fff2324000dd0c2674190303c
Axel <loui.molina1@gmail.com>
2021-02-27 20:16:16 -0300

    Unpush local config

--- openApiPackage.bat
+++ openApiPackage.bat
@@ -1,1 +1,1 @@
-curl.exe --output open-api.json --url http://localhost:3001/api-json && openapi-generator-cli generate -g typescript-angular -i ./open-api.json -o ../banca-fe/local-packages/banca-api  --additional-properties npmName=banca-api && del open-api.json
+curl.exe --output open-api.json --url http://localhost:3000/api-json && openapi-generator-cli generate -g typescript-angular -i ./open-api.json -o ../banca-fe/local-packages/banca-api  --additional-properties npmName=banca-api && del open-api.json
