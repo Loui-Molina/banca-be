d23d7702fd1322f833f1f5a69057c48336c8831c
Gonzalo Catrina <gonzalocatrina@gmal.com>
2021-03-19 19:11:03 -0300

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@src/app.module';
 import { AnyExceptionFilter } from '@common/filters/any.exception.filter';
 import { ValidationPipe } from '@common/pipes/validation.pipe';
 import { AppLogger } from '@common/logger/app.logger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.enableCors();
     app.set('trust proxy', true);
+    app.use(helmet());
     app.useLogger(new AppLogger());
     app.setGlobalPrefix(app.get('ConfigService').get('APP_GLOBAL_PREFIX'));
     app.useGlobalFilters(new AnyExceptionFilter());
     app.useGlobalPipes(new ValidationPipe());
-    const options = new DocumentBuilder()
+    /*const options = new DocumentBuilder()
         .setTitle(app.get('ConfigService').get('APP_TITLE'))
         .setDescription(app.get('ConfigService').get('APP_DESCRIPTION'))
         .setVersion(app.get('ConfigService').get('APP_VERSION'))
         .addTag(app.get('ConfigService').get('APP_TAG'))
         .build();
     const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup(app.get('ConfigService').get('APP_SWAGGER_SETUP'), app, document);
+    SwaggerModule.setup(app.get('ConfigService').get('APP_SWAGGER_SETUP'), app, document);*/
 
     await app.listen(/*process.env.PORT || */ 3000);
 }
