4dd00f384220f25a296a8829a66a363bd02d5cdc
Sebastian Imvinkelried <sebin1997@hotmail.com>
2021-03-22 22:40:25 -0300

    Posible fix qsy

--- src/modules/lotteries/banking/banking.lottery.service.ts
+++ src/modules/lotteries/banking/banking.lottery.service.ts
@@ -53,9 +53,13 @@ export class BankingLotteryService {
                     }
                 });
                 if (flag) {
+                    // const a = new Date('1970-01-02T' + new Date().toISOString().split('T')[1]);
                     const lotteryOpenTime: Date = new Date(lottery.openTime);
                     const lotteryCloseTime: Date = new Date(lottery.closeTime);
-                    const now = new Date('1970-01-01T' + new Date().toISOString().split('T')[1]);
+                    const now = new Date();
+                    now.setFullYear(1970, 0, 1);
+                    // lotteryOpenTime = new Date(lotteryOpenTime.getTime() + now.getTime());
+                    // lotteryCloseTime = new Date(lotteryCloseTime.getTime() + now.getTime());
                     let leftTime = (lotteryCloseTime.getTime() - now.getTime()) / 1000;
                     if (!(lotteryOpenTime <= now && lotteryCloseTime >= now)) {
                         lottery.status = false;
