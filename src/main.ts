a05dd41409a5c4bb528c8d3389eedb0bb89b7a3c
loui <loui.molina1@gmail.com>
2021-04-04 20:52:19 -0400

    Create dev flag for use locally

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@common/pipes/validation.pipe';
 import { AppLogger } from '@common/logger/app.logger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as helmet from 'helmet';
+import { install } from 'source-map-support';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,16 +17,21 @@ async function bootstrap() {
     app.setGlobalPrefix(app.get('ConfigService').get('APP_GLOBAL_PREFIX'));
     app.useGlobalFilters(new AnyExceptionFilter());
     app.useGlobalPipes(new ValidationPipe());
-    /*const options = new DocumentBuilder()
-        .setTitle(app.get('ConfigService').get('APP_TITLE'))
-        .setDescription(app.get('ConfigService').get('APP_DESCRIPTION'))
-        .setVersion(app.get('ConfigService').get('APP_VERSION'))
-        .addTag(app.get('ConfigService').get('APP_TAG'))
-        .build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup(app.get('ConfigService').get('APP_SWAGGER_SETUP'), app, document);*/
 
-    await app.listen(/*process.env.PORT || */ 3000);
+    let port: number = parseInt(process.env.PORT);
+    if (process.env.DEV) {
+        install();
+        const options = new DocumentBuilder()
+            .setTitle(app.get('ConfigService').get('APP_TITLE'))
+            .setDescription(app.get('ConfigService').get('APP_DESCRIPTION'))
+            .setVersion(app.get('ConfigService').get('APP_VERSION'))
+            .addTag(app.get('ConfigService').get('APP_TAG'))
+            .build();
+        const document = SwaggerModule.createDocument(app, options);
+        SwaggerModule.setup(app.get('ConfigService').get('APP_SWAGGER_SETUP'), app, document);
+        port = 3000;
+    }
+    await app.listen(port);
 }
 
 // noinspection JSIgnoredPromiseFromCall
