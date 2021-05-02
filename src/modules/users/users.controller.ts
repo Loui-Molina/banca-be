1b14985529b7aac3926f0764b1bbf7a5013929d0
gcatrinakw <gonzalocatrina@gmail.com>
2021-05-01 22:23:57 -0300

    deleted console log from user deleted

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -65,7 +65,6 @@ export class UsersController {
         type: User,
     })
     delete(@Param('id') id: mongoose.ObjectId): Promise<User> {
-        console.log(id);
         return this.userService.delete(id);
     }
 
